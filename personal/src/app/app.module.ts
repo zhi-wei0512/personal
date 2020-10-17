635924d6bdc36c97895e53021d5a16ae083e642b
MSI\何致緯 <a0983393895@gmail.com>
2020-10-17 09:38:59 +0800

    #master 第一次新創專案

---
+++ personal/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './personal/model/template/template.component';
+import { MenuComponent } from './personal/model/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
